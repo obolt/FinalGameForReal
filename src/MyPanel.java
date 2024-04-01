ff6a94bc710ebd12189484332b4ed276937a2d4d
elizabeth.meyer25 <elizabeth.meyer25@gmail.com>
2024-04-01 11:49:26 -0500

    tried ball class

---
+++ src/MyPanel.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.*;
+
+// import static jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle.circle;
+// ^where did that come from
+
+public class MyPanel extends JPanel {
+
+    int xLoc = 200;
+    int yLoc = 300;
+
+    int xVel = 10;
+    public MyPanel(){
+
+       // setBackground(Color.GREEN);
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+//        System.out.println("Hi!");
+        g.fillOval(20,30,5,8);
+        g.fillOval(400, 400, 5, 8);
+        g.setColor(Color.RED);
+        g.fillOval(xLoc, yLoc, 20, 20);
+////        xLoc +=2;
+//        xLoc += xVel;
+//        //yLoc -=4;
+//
+//        if (xLoc > getWidth()-20){
+//            xVel=  xVel*-1;
+//        }
+//        if( xLoc < 20){
+//            xVel = xVel*-1;
+//        }
+
+
+        g.setColor(Color.blue);
+        g.drawString("Hello World", 0, 50);
+        JButton button = new JButton();
+        button.setIcon(new ImageIcon(circle.png));
+        try {
+            Thread.sleep(100);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        repaint();
+    }
+
+
+}
